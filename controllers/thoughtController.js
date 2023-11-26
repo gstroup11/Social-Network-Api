f919c619cd5ddb1e53cb309099434a1d34f1e7e6
gstroup11 <gstroup11@gmail.com>
2023-11-26 17:00:44 -0500

    This adds in all routes and controllers for /api/thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,19 +1,104 @@
-const { Thought, User } = require('../models');
+const { Thought, User } = require("../models");
+const { create } = require("../models/user");
 
 const thoughtController = {
+  //Get all thoughts
+  async getAllThoughts(req, res) {
+    try {
+      const dbThoughtData = await Thought.find().sort({ createdAt: -1 });
 
-    //Get all thoughts
-    async getAllThoughts (req, res) {
-        try {
-            const dbThoughtData = await Thought.find()
-              .sort({ createdAt: -1 });
-      
-            res.json(dbThoughtData);
-          } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-          }
-        },
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // Get one thought by id
+  async getThoughtById(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOne({ _id: req.params.id });
+
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this id!" });
+        return;
+      }
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Create a thought
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // Update a thought by id
+  async updateThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+      );
+
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this id!" });
+        return;
+      }
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  
+// Delete a thought by id
+  async deleteThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndDelete({
+        _id: req.params.id,
+      });
+
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this id!" });
+        return;
+      }
+
+      const dbUserData = User.findOneAndUpdate(
+        { thoughts: req.params.thoughtId },
+        { $pull: { thoughts: req.params.thoughtId } },
+        { new: true }
+      );
+
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+
+      res.json({ message: "Thought successfully deleted!" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const {
     getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
 } = require('../../controllers/thoughtController');
 
-router.route('/').get(getAllThoughts);
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/:id').get(getThoughtById).post(updateThought).delete(deleteThought);
 
 module.exports = router;
 
