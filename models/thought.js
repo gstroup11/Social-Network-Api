71faa3ee644ea9f00d27458086623b52b173dc42
gstroup11 <gstroup11@gmail.com>
2023-11-26 12:12:23 -0500

    Adds in the thoughts model, and moment to use for the timestamp

---
+++ models/thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAt) => moment(createdAt).format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called reactionCount
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Create the Thought model using the thoughtSchema
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
